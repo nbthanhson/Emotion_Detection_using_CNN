be7ebe1235c6623644475110237256c40547b0c7
Sơn Nguyễn <83537172+thanhson1604@users.noreply.github.com>
2021-06-18 17:26:22 +0700

    Update model.py

--- model.py
+++ model.py
@@ -5,12 +5,11 @@ from keras.models import Sequential
 model = Sequential()
 
 #Add the first Block
-model.add(Conv2D(filters = 32, kernel_size = (3, 3), padding = 'same', activation = 'relu', input_shape = train_data.shape[1:]))
+model.add(Conv2D(filters = 32, kernel_size = (3, 3), padding = 'same', activation = 'relu', input_shape = train_x.shape[1:]))
 model.add(BatchNormalization())
 model.add(Conv2D(filters = 32, kernel_size = (3, 3), padding = 'same', activation = 'relu'))
 model.add(BatchNormalization())
 model.add(MaxPool2D(pool_size = (2, 2)))
-model.add(Dropout(0.2))
 
 #Add the second Block
 model.add(Conv2D(filters = 64, kernel_size = (3, 3), padding = 'same', activation = 'relu'))
@@ -18,7 +17,6 @@ model.add(BatchNormalization())
 model.add(Conv2D(filters = 64, kernel_size = (3, 3), padding = 'same', activation = 'relu'))
 model.add(BatchNormalization())
 model.add(MaxPool2D(pool_size = (2, 2)))
-model.add(Dropout(0.2))
 
 #Add the third Block
 model.add(Conv2D(filters = 128, kernel_size = (3, 3), padding = 'same', activation = 'relu'))
@@ -26,7 +24,6 @@ model.add(BatchNormalization())
 model.add(Conv2D(filters = 128, kernel_size = (3, 3), padding = 'same', activation = 'relu'))
 model.add(BatchNormalization())
 model.add(MaxPool2D(pool_size = (2, 2)))
-model.add(Dropout(0.2))
 
 #Add the four Block
 model.add(Conv2D(filters = 256, kernel_size = (3, 3), padding = 'same', activation = 'relu'))
@@ -34,7 +31,6 @@ model.add(BatchNormalization())
 model.add(Conv2D(filters = 256, kernel_size = (3, 3), padding = 'same', activation = 'relu'))
 model.add(BatchNormalization())
 model.add(MaxPool2D(pool_size = (2, 2)))
-model.add(Dropout(0.2))
 
 #Add the Flatten layer
 model.add(Flatten())
@@ -55,18 +51,18 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
 #Early stop training
-early_stopping = EarlyStopping(monitor = 'val_loss', patience = 10, verbose = 1)
+early_stopping = EarlyStopping(monitor = 'val_loss', patience = 15, verbose = 1)
 
 #Save the best model
 best_model = ModelCheckpoint(filepath = 'Emotion_Detection_bestmodel.h5', monitor = 'val_loss', verbose = 1, save_best_only = True)
 
 #Reduce learning rate
-reduce_lr = ReduceLROnPlateau(monitor = 'val_loss', factor = 0.2, patience = 5, verbose = 1, min_lr = 0.0001)
+reduce_lr = ReduceLROnPlateau(monitor = 'val_loss', factor = 0.1, patience = 10, verbose = 1, min_lr = 0.0001)
 
 #Start training
 model_history = model.fit(
-                        data_generator.flow(train_data, train_label, batch_size = 64),
-                        epochs = 50,
+                        data_generator.flow(train_x, train_y, batch_size = 128),
+                        epochs = 100,
                         validation_data = val_data,
                         callbacks = [early_stopping, best_model, reduce_lr],
                         shuffle = True)
